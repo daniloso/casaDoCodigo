1bf4e145b6cad391b357b3bfc870db59b47cb9c0
daniloso <daniloso20@gmail.com>
2019-02-13 17:36:16 -0200

    Validando os numeros primos

--- Livraria/src/TesteNumPrimo.java
+++ Livraria/src/TesteNumPrimo.java
@@ -4,17 +4,20 @@ public class TesteNumPrimo {
 	public static void main(String[] args) {
 
 		Scanner entradaDeNum = new Scanner(System.in);
-		
-		double num;
+
+		int num;
 		int i = 2;
-		System.out.println("Digite um numero:");
 		
-		num = entradaDeNum.nextDouble();
-		
-		if (num % i == 0) {
-			System.out.println("numero primo");
-		}else {
-			System.out.println("Numero não primo");
+		System.out.println("Digite um numero:\n");
+		num = entradaDeNum.nextInt();
+		for (i = 2; i <= num; i++) {
+			if (num % i == 0) {
+				System.out.println("O numero " + num + " é divisivel por " + i + ", portanto é primo!");
+			} else {
+				System.out.println("O numero " + num + " não é divisivel por " + i + ", portanto não é primo!");
+			}
+//			System.out.println(i);
 		}
+
 	}
 }
